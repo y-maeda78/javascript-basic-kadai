d65be3e2e06fb849863758baccbe2d17eecc7138
y-maeda78 <prknowledge.my78@gmail.com>
2025-08-07 23:32:02 +0900

    kadai_004を追加

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+//loadイベント
+$(window).on('load', function () {
+  console.log('loadイベントが発生しました');
+});
+
+//scrollイベント
+$(window).on('scroll', function () {
+  console.log('scrollイベントが発生しました');
+});
